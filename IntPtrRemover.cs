ade525f60878db2571826f84c7c181070e60d28c
DevT02 <40608267+DevT02@users.noreply.github.com>
2020-08-31 18:17:53 -0400

    added source files
    source files, not uploading the entire project but will add a rel

---
+++ IntPtrRemover.cs
@@ -0,0 +1,67 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntptrPoint
+{
+    class IntPtrRemover
+    {
+        public static int amount = 0;
+        public static void Clean()
+        {
+            foreach (TypeDef types in Program.module.GetTypes())
+            {
+                foreach (MethodDef method in types.Methods)
+                {
+                    if (!method.HasBody) continue;
+                    IList<Instruction> instr = method.Body.Instructions;
+                    for (int i = 0; i < instr.Count; i++)
+                    {
+                        if (method.Body.Instructions[i].OpCode == OpCodes.Call && (instr[i].Operand.ToString().ToLower().Contains("intptr::subtract") || instr[i].Operand.ToString().ToLower().Contains("intptr::add")) && instr[i - 1].OpCode.ToString().Contains("ldc.i4") && instr [i - 2].Operand.ToString().ToLower().Contains("intptr::op_explicit") && instr[i - 3].OpCode.ToString().Contains("ldc.i4"))
+                        {
+                            /// CODE: IntPtr.Add((IntPtr)1, 259)
+                            /// In Dnspy:
+                            // 15  0037    ldc.i4  1
+                            // 16  003C    call    native int[mscorlib] System.IntPtr::op_Explicit(int32)
+                            // 17  0041    ldc.i4  0x103 
+                            // 18  0046    call native int[mscorlib] System.IntPtr::Add(native int, int32)
+
+                            /// NOTE: 0x103 = 259
+                            // op_Explicit is (IntPtr)
+                            // IntPtr::Add is IntPtr.Add
+                            // Compiling this in an online compiler will result in 260. IntPtr mutations don't prioritize pointers. In other words, this statement is literally just 1 + 259 = 260.  
+                            // Stil not convinced? https://offline.is-ne.at/vayAi8   https://offline.is-ne.at/P2kHoN
+                            // Same goes for IntPtr::Subtract. (except the adding part ofc)                   
+
+                            int y = instr[i - 1].GetLdcI4Value(), x = instr[i - 3].GetLdcI4Value(), result;
+                            if (instr[i].Operand.ToString().ToLower().Contains("intptr::subtract"))
+                            {
+                                result = x - y;
+                                instr[i].OpCode = OpCodes.Nop;
+                                instr[i - 1].OpCode = OpCodes.Nop;
+                                instr[i - 2].OpCode = OpCodes.Nop;                              
+                                instr[i - 3].Operand = result;
+                                amount++;
+                            }
+                            else if(instr[i].Operand.ToString().ToLower().Contains("intptr::add"))
+                            {
+                                result = x + y;
+                                instr[i].OpCode = OpCodes.Nop;
+                                instr[i - 1].OpCode = OpCodes.Nop;
+                                instr[i - 2].OpCode = OpCodes.Nop;
+                                instr[i - 3].Operand = result;
+                                amount++;
+                            }
+       
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using de4dot.blocks;
+using de4dot.blocks.cflow;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntptrPoint
+{
+    class Program
+    {
+        public static string Asmpath;
+        public static ModuleDefMD module;
+        public static Assembly asm;
+
+        private static bool PointMutations = true;
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "IntPtr Fixer by OFF_LINE";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n");
+            string lol = @" ______              __      _______     __                ________  __    __ 
+/      |            /  |    /       \   /  |              /        |/  |  /  |
+$$$$$$/  _______   _$$ |_   $$$$$$$  | _$$ |_     ______  $$$$$$$$/ $$ |  $$ |
+  $$ |  /       \ / $$   |  $$ |__$$ |/ $$   |   /      \ $$ |__    $$  \/$$/ 
+  $$ |  $$$$$$$  |$$$$$$/   $$    $$/ $$$$$$/   /$$$$$$  |$$    |    $$  $$<  
+  $$ |  $$ |  $$ |  $$ | __ $$$$$$$/    $$ | __ $$ |  $$/ $$$$$/      $$$$  \ 
+ _$$ |_ $$ |  $$ |  $$ |/  |$$ |        $$ |/  |$$ |      $$ |       $$ /$$  |
+/ $$   |$$ |  $$ |  $$  $$/ $$ |        $$  $$/ $$ |      $$ |      $$ |  $$ |
+$$$$$$/ $$/   $$/    $$$$/  $$/          $$$$/  $$/       $$/       $$/   $$/ 
+                                                                              
+                                                                              
+                                                                              ";
+            Console.WriteLine(lol);
+            string diretorio = args[0];
+            try
+            {
+                Program.module = ModuleDefMD.Load(diretorio, (ModuleCreationOptions) null);
+                Program.asm = Assembly.LoadFrom(diretorio);
+                Program.Asmpath = diretorio;
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Not .NET Assembly...");
+            }
+            string text = Path.GetDirectoryName(diretorio);
+            bool flag = !text.EndsWith("\\");
+            bool flag2 = flag;
+            if (flag2)
+            {
+                text += "\\";
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Successfully loaded assembly!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        
+            try
+            {
+                Console.WriteLine("Removing INTPTR mutations!");
+                IntptrPoint.IntPtrRemover.Clean();
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[!] Failed to remove mutations. ORIGINAL EXCEPTION: " + ex.ToString());
+
+            }
+            string filename = string.Format("{0}{1}_RemovedPoint{2}", text, Path.GetFileNameWithoutExtension(diretorio), Path.GetExtension(diretorio));
+            ModuleWriterOptions writerOptions = new ModuleWriterOptions(Program.module);
+            writerOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            NativeModuleWriterOptions NativewriterOptions = new NativeModuleWriterOptions(Program.module);
+            NativewriterOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            NativewriterOptions.Logger = DummyLogger.NoThrowInstance;
+            bool isILOnly = Program.module.IsILOnly;
+            if (isILOnly)
+            {
+                Program.module.Write(filename, writerOptions);
+            }
+            else
+            {
+                Program.module.NativeWrite(filename, NativewriterOptions);
+            }
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Successfully removed " + IntPtrRemover.amount + " IntPtr mutations");
+            Console.ReadLine();
+        }
+    }
+}
